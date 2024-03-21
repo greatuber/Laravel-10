4ea17b8bbf1e72a02568f335037ccd2c21892d7b
greatuber123 <greatuber123@gmail.com>
2024-03-21 09:31:40 -0500

    criando rotas controllers e views

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,8 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\{SupportController};
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
 
 Route::get('/', function () {
     return view('welcome');
